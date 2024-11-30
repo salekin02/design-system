0290c76c78b5c96567eda5780fc507518be7de10
Nahid Salekin <36966165+salekin02@users.noreply.github.com>
2024-11-30 14:19:27 +0600

    test

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,10 @@ import dts from 'vite-plugin-dts';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), dts()],
+  plugins: [react(), dts({
+    outDir: 'dist',
+    insertTypesEntry: true,
+  })],
   build: {
     lib: {
       entry: 'src/index.ts',
@@ -23,6 +26,6 @@ export default defineConfig({
       },
     },
     minify: true,
-    sourcemap: false, 
+    sourcemap: false,
   },
 })
